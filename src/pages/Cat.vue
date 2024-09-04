adf7aaeb07f77c8ae35e7ccd8fa8b5beeed587f7
TurboHsu <47477044+TurboHsu@users.noreply.github.com>
2024-09-04 20:38:15 +0800

    ✨ fine not found

--- src/pages/Cat.vue
+++ src/pages/Cat.vue
@@ -1,33 +1,63 @@
 <script setup lang="ts">
-import ChunkCatView from '@/components/ChunkCatView.vue';
-import Card from '@/components/ui/card/Card.vue';
-import CardContent from '@/components/ui/card/CardContent.vue';
-import CardHeader from '@/components/ui/card/CardHeader.vue';
-import { Cat } from '@/types/cat';
-import { fetchCat } from '@/utils/api/api';
-import { Icon } from '@iconify/vue';
-import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
-
+import ChunkCatView from "@/components/ChunkCatView.vue";
+import Card from "@/components/ui/card/Card.vue";
+import CardContent from "@/components/ui/card/CardContent.vue";
+import CardHeader from "@/components/ui/card/CardHeader.vue";
+import { Cat } from "@/types/cat";
+import { fetchCat } from "@/utils/api/api";
+import { Icon } from "@iconify/vue";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { toast } from "vue-sonner";
+import { Button } from "@/components/ui/button";
 
 const route = useRoute();
 const cat = ref<Cat>();
 
-onMounted(async () => {
-    const catId = route.params.uuid as string;
-    cat.value = await fetchCat(catId);
-});
+const goBack = () => {
+	window.history.back();
+};
 
+onMounted(async () => {
+	const catId = route.params.uuid as string;
+	try {
+		cat.value = await fetchCat(catId);
+	} catch (e: any) {
+		console.error(e);
+		if (e?.response?.status === 404) {
+			toast.error("No such cat");
+		}
+	}
+});
 </script>
 
 <template>
-    <Card class="mx-10 my-10">
-        <CardHeader class="flex flex-row flex-wrap items-center gap-5">
-            <Icon icon="fa6-solid:cat" class="w-8 h-8" />
-            <span class="break-all ">Cat ID: {{ route.params.uuid }}</span>
-        </CardHeader>
-        <CardContent>
-            <ChunkCatView v-if="cat" :data="cat" />
-        </CardContent>
-    </Card>
+	<Card class="mx-10 my-10">
+		<CardHeader class="flex flex-row flex-wrap items-center gap-5">
+			<Icon icon="fa6-solid:cat" class="w-8 h-8" />
+			<span class="break-all">Cat ID: {{ route.params.uuid }}</span>
+		</CardHeader>
+		<CardContent>
+			<ChunkCatView v-if="cat" :data="cat" />
+			<div v-else class="w-full h-[60vh] flex flex-col items-center justify-center">
+				<div class="flex flex-col justify-center w-full my-2">
+                    <div class="flex justify-center w-full">
+                        <Icon icon="line-md:alert-circle-twotone" class="w-16 h-16 text-orange-400" />
+                    </div>
+					<p class="text-2xl font-semibold text-center dark:text-gray-100">
+						No such cat...
+					</p>
+					<p class="text-lg text-center text-gray-500 dark:text-grey-400">
+						Poor kitty... Let's praise the cat gods for its safe return home.
+					</p>
+				</div>
+				<div class="flex items-center justify-center w-full my-2">
+					<Button class="items-center" @click="goBack">
+						<Icon icon="ion:chevron-back" class="mr-2 size-4" />
+						<div>Go back</div>
+					</Button>
+				</div>
+			</div>
+		</CardContent>
+	</Card>
 </template>
--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts">
-import { Icon } from '@iconify/vue';
-import { Button } from '@/components/ui/button';
+import { Icon } from "@iconify/vue";
+import { Button } from "@/components/ui/button";
 
 function goBack() {
-    window.history.back();
+	window.history.back();
 }
 </script>
 
 <template>
-    <div class="flex w-full h-[80vh] items-center justify-center flex-col">
-        <div class="w-full flex justify-center my-2">
-            <Icon icon="noto-v1:thinking-face" class="w-[10rem] h-[10rem]" />
-        </div>
-        <div class="w-full flex justify-center my-2 flex-col">
-            <p class="text-center text-gray-100 text-2xl font-semibold">Nothing here...</p>
-            <p class="text-center text-gray-400 text-lg">Looks like this kitty wandered too far from home!</p>
-        </div>
-        <div class="w-full flex justify-center my-2 items-center">
-            <Button class="items-center" @click="goBack" >
-                <Icon icon="ion:chevron-back" class="mr-2 size-4" />
-                <div>Go back</div>
-            </Button>
-        </div>
-    </div>
+	<div class="flex w-full h-[80vh] items-center justify-center flex-col">
+		<div class="flex justify-center w-full my-2">
+			<Icon icon="noto-v1:thinking-face" class="w-[10rem] h-[10rem]" />
+		</div>
+		<div class="flex flex-col justify-center w-full my-2">
+			<p class="text-2xl font-semibold text-center dark:text-gray-100">
+				Nothing here...
+			</p>
+			<p class="text-lg text-center text-gray-500 dark:text-gray-400">
+				Looks like this kitty wandered too far from home!
+			</p>
+		</div>
+		<div class="flex items-center justify-center w-full my-2">
+			<Button class="items-center" @click="goBack">
+				<Icon icon="ion:chevron-back" class="mr-2 size-4" />
+				<div>Go back</div>
+			</Button>
+		</div>
+	</div>
 </template>
